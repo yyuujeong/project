55e4f0c1cfa10340a6c0e7491e633718abe13e90
yyuujeong <dlfhd.217@gmail.com>
2022-05-28 22:14:58 +0900

    추가 카테고리 재수정

--- stargram_renewal/js/add.js
+++ stargram_renewal/js/add.js
@@ -1,44 +1,7 @@
-//팔로우 클릭
-const follow = document.getElementsByClassName('follow');
-const modal = document.getElementById('modal');
-
-function onFollow(e) {
-    modal.style.display = 'block';
-}
-function offFollow() {
-    modal.style.display = 'none';
-}
-
-//추가 카테고리 
-function onBtn() {
-    //addCategory.style.display = 'block';
-    //addCategory.classList.add('animated', 'fadeInUp');
-
-    // addCategory.animate([
-    //     { opacity: "0", transform: "translateX(-50%), translateY(20px)" },
-    //     { opacity: "1", transform: "translateX(0), translateY(0)" }
-    // ], {
-    //     duration: 200
-    // }); 
-
-    // addCategory.style.display = 'block';
-
-    // if(btnPlus.clicked == true) {
-    //     addCategory.classList.add('animated', 'fadeInUp');
-    // } else {
-    //     addCategory.classList.remove('animated', 'fadeInUp');
-    // }
-
-    let addCategory = document.getElementById('add-category');
-
-    if(addCategory.style.display = 'block') {
-        addCategory.classList.add('animated', 'fadeInUp');
-    } else {
-        addCategory.classList.remove('animated', 'fadeInUp');
-    }  
-}
 
 //추가 버튼 클릭 시
+const txtInput = document.getElementById("txtInput");
+const addBtn = document.getElementById("add-btn");
 
 
 
